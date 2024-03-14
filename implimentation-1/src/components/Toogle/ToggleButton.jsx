3d7aaf434892d79b0c8bddddf816b1b95fa9b206
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-03-14 22:40:54 +0530

    toggle

---
+++ implimentation-1/src/components/Toogle/ToggleButton.jsx
@@ -0,0 +1,32 @@
+// Create a functional component that implements a toggle button using the useState hook. The button should toggle between "On" and "Off" when clicked, and the initial state should be "Off". Additionally, style the button to visually represent its current state.
+
+import React, { useState } from "react";
+import "./ToggleButton.css";
+
+const ToggleButton = () => {
+  const [isToggle, setIsToggle] = useState(false);
+
+  const checkToggle = () => {
+    setIsToggle(!isToggle);
+  };
+
+  return (
+    <div className="toggle-button">
+      <p className="text">
+        Create a functional component that implements a toggle button using the
+        useState hook. The button should toggle between "On" and "Off" when
+        clicked, and the initial state should be "Off". Additionally, style the
+        button to visually represent its current state.
+      </p>
+      <button
+        className={isToggle ? "toggle-button-on" : "toggle-button-off"}
+        onClick={checkToggle}
+      >
+        {isToggle ? "On" : "Off"}
+      </button>{" "}
+      <span>← Please Click and Check. </span>
+    </div>
+  );
+};
+
+export default ToggleButton;
