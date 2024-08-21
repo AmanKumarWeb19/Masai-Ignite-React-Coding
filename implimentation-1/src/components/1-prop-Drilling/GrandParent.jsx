efea39b4e51146faa551fd48db3ac1fd0bbf49cd
Aman Kumar <akgupta0524@gmail.com>
2024-08-21 07:40:48 +0530

    practice prop drilling

--- implimentation-1/src/components/1-prop-Drilling/GrandParent.jsx
+++ implimentation-1/src/components/1-prop-Drilling/GrandParent.jsx
@@ -1,14 +1,39 @@
+// import React from "react";
+// import ParentComponent from "./ParentComponent";
+
+// const GrandParent = () => {
+//   let message = "Welcome to check prop Drilling..!";
+//   return (
+//     <>
+//       <div>
+//         <ParentComponent message={message} />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default GrandParent;
 import React from "react";
 import ParentComponent from "./ParentComponent";
 
 const GrandParent = () => {
-  let message = "Welcome to check prop Drilling..!";
+  let message = "Welcome to PropDrilling World .....!";
   return (
-    <>
-      <div>
-        <ParentComponent message={message} />
-      </div>
-    </>
+    <div
+      style={{
+        border: "2px solid red",
+        width: "50%",
+        padding: "15px",
+        textAlign: "center",
+        margin: "auto",
+        fontSize: "30px",
+        fontWeight: "bold",
+        fontStyle: "italic",
+        marginTop: "50Px",
+      }}
+    >
+      <ParentComponent message={message} />
+    </div>
   );
 };
 
--- implimentation-1/src/components/1-prop-Drilling/ParentComponent.jsx
+++ implimentation-1/src/components/1-prop-Drilling/ParentComponent.jsx
@@ -1,10 +1,23 @@
+// import React from "react";
+// import ChildComponent from "./ChildComponent";
+
+// const ParentComponent = ({ message }) => {
+//   return (
+//     <div>
+//       <h1>Props Drilling...</h1>
+//       <ChildComponent message={message} />
+//     </div>
+//   );
+// };
+
+// export default ParentComponent;
+
 import React from "react";
 import ChildComponent from "./ChildComponent";
 
 const ParentComponent = ({ message }) => {
   return (
     <div>
-      <h1>Props Drilling...</h1>
       <ChildComponent message={message} />
     </div>
   );
